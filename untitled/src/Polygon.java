cf3ff004cf7e3c6d7a19798d03d595b0df85ab73
sublem <matveytop04@gmail.com>
2026-03-06 11:42:38 +0100

    zadania lab 2 zad 4

--- untitled/src/Polygon.java
+++ untitled/src/Polygon.java
@@ -6,7 +6,10 @@ public class Polygon {
 
     public Polygon(Point[] points) {
         this.points = new Point[points.length];
-        System.arraycopy(points,0,this.points,0,points.length);
+        for (int i=0;i<points.length;i++)
+        {
+            this.points[i] = new Point(points[i]);
+        }
     }
 
     @Override
