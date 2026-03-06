66f5d6781fc3bf5195f6b73238e7f8ac7885a97b
sublem <matveytop04@gmail.com>
2026-03-06 11:30:26 +0100

    zadania lab 2 zad 4

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,19 +5,14 @@ public class Main
 
     public static void main(String[] args)
     {
-
-        Point p = new Point();
-
-        System.out.println(p);
-        p.setX(5);
-        System.out.println(p);
-
-        Point p2 = new Point(2.6F,9);
-        System.out.println(p2);
-
-        Segment s1 = new Segment(new Point(p),new Point(p2));
-        System.out.println(s1);
-        p.setX(100);
-        System.out.println(s1);
+        Point[] points = new Point[5];
+        points[0] = new Point(2.3F,5.6F);
+        points[1] = new Point(10.5F, 12.4F);
+        points[2] = new Point(15.4F,74.2F);
+        points[3] = new Point(72.4F,81.6F);
+        points[4] = new Point(162.3F,51.3F);
+        Polygon p1 = new Polygon(points);
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
     }
 }
---
+++ untitled/src/Polygon.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Polygon {
+    private Point[] points;
+
+    public Polygon(Point[] points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Polygon{poinst = " + Arrays.toString(points) + "}";
+    }
+
+    public String toSvg()
+    {
+    String pointstring = "";
+    for (Point point : points) {
+        pointstring += point.getX() + "," + point.getY() + " ";
+    }
+    return String.format(Locale.ENGLISH, "<polygon points=\"%s\" style=\"fill:lime;stroke:purple;stroke-width:3\" >", pointstring);
+    }
+}
+
